ee7df9c4ec026fbd280a6708d7894a2946999315
JessieBee7 <jessicaelammiller@gmail.com>
2025-02-26 12:14:54 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
- env: {
-   browser: true,
-   es2021: true,
- },
- extends: [
-   'eslint:recommended',
-   'plugin:react/recommended',
-   'plugin:react-hooks/recommended',
- ],
- parserOptions: {
-   ecmaVersion: 'latest',
-   sourceType: 'module',
- },
- plugins: ['react'],
- rules: {
-   'react/react-in-jsx-scope': 'off',
-   'no-unused-vars': 'warn',
-   'no-undef': 'warn',
- },
- settings: {
-   react: {
-     version: 'detect',
-   },
- },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
+    'react/prop-types': 'off'  // Disables the prop-types validation
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
