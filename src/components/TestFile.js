dd8028e6f7091585c6f9191b95da39d61fb0a3d2
JessieBee7 <jessicaelammiller@gmail.com>
2025-03-05 10:34:33 -0500

    Create TestFile.js

---
+++ src/components/TestFile.js
@@ -0,0 +1,10 @@
+// src/components/TestFile.js
+import React from 'react';
+
+const TestFile = () => (
+  <div className="p-4 bg-red-500 text-white text-center rounded-lg font-bold">
+    THIS IS A TEST FILE - VERSION 1.0
+  </div>
+);
+
+export default TestFile;
